1145a9ada6f7627fbc20ff5538ae01d008419f9d
3000Coder <173622885+3000Coder@users.noreply.github.com>
2024-07-07 22:24:11 +0200

    Another refactor

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,46 @@ use std::fs::{self};
 use std::io;
 use std::path::PathBuf;
 
-const SHOW_HIDDEN: bool = true;
+const SHOW_HIDDEN: bool = false;
 
-enum DirectoryEntery {
-    Directory { name: String, is_hidden: bool },
-    File { name: String, is_hidden: bool },
+#[derive(Clone)]
+enum EntryType {
+    File,
+    Directory,
+    BlockDevice,
+    CharDevice,
+    Fifo,
+    Socket,
+    SymlinkFile,
+    SymlinkDir,
+}
+
+struct DirectoryEntery {
+    name: String,
+    entry_type: EntryType,
+    is_hidden: bool,
+}
+
+impl DirectoryEntery {
+    pub fn name(&self) -> &String {
+        return &self.name;
+    }
+
+    pub fn entry_type(&self) -> &EntryType {
+        return &self.entry_type;
+    }
+
+    pub fn is_hidden(&self) -> &bool {
+        return &self.is_hidden;
+    }
+
+    pub fn new(name: &String, entry_type: &EntryType, is_hidden: &bool) -> Self {
+        return Self {
+            name: name.to_string().clone(),
+            entry_type: entry_type.clone(),
+            is_hidden: is_hidden.clone(),
+        };
+    }
 }
 
 fn is_hidden(s: &str) -> bool {
@@ -24,16 +59,19 @@ fn parse_pathbuffs(files: &Vec<PathBuf>) -> Vec<DirectoryEntery> {
     for file in files {
         if let Some(f) = file.file_name() {
             if let Some(filename) = f.to_str() {
+                // TODO: Detect more types, move to function
                 if file.is_file() {
-                    res_buf.push(DirectoryEntery::File {
-                        name: filename.to_string(),
-                        is_hidden: is_hidden(filename),
-                    })
+                    res_buf.push(DirectoryEntery::new(
+                        &filename.to_string(),
+                        &EntryType::File,
+                        &is_hidden(filename),
+                    ));
                 } else {
-                    res_buf.push(DirectoryEntery::Directory {
-                        name: filename.to_string(),
-                        is_hidden: is_hidden(filename),
-                    })
+                    res_buf.push(DirectoryEntery::new(
+                        &filename.to_string(),
+                        &EntryType::Directory,
+                        &is_hidden(filename),
+                    ));
                 }
             }
         }
@@ -42,25 +80,17 @@ fn parse_pathbuffs(files: &Vec<PathBuf>) -> Vec<DirectoryEntery> {
     return res_buf;
 }
 
+// ! Assuming name isn't empty
 fn get_entry_name_without_dot(entry: &DirectoryEntery) -> String {
-    match entry {
-        DirectoryEntery::File {
-            name: n,
-            is_hidden: hidden,
-        }
-        | DirectoryEntery::Directory {
-            name: n,
-            is_hidden: hidden,
-        } => {
-            if *hidden {
-                let mut chars = n.chars();
-                chars.next();
-                chars.next_back();
-                return chars.as_str().to_string();
-            } else {
-                return n.to_string();
-            }
-        }
+    let hidden = entry.is_hidden();
+    let name = entry.name();
+    if *hidden {
+        let mut chars = name.chars();
+        chars.next();
+        chars.next_back();
+        return chars.as_str().to_string();
+    } else {
+        return name.to_string();
     }
 }
 
@@ -74,34 +104,31 @@ fn main() -> io::Result<()> {
     let mut p_enteries: Vec<DirectoryEntery> = parse_pathbuffs(&entries);
 
     if SHOW_HIDDEN {
-        p_enteries.push(DirectoryEntery::Directory {
-            name: String::from("."),
-            is_hidden: true,
-        });
-        p_enteries.push(DirectoryEntery::Directory {
-            name: String::from(".."),
-            is_hidden: true,
-        });
+        p_enteries.push(DirectoryEntery::new(
+            &String::from("."),
+            &EntryType::Directory,
+            &false,
+        ));
+        p_enteries.push(DirectoryEntery::new(
+            &String::from(".."),
+            &EntryType::Directory,
+            &false,
+        ));
     }
 
     p_enteries.sort_by_key(|a| get_entry_name_without_dot(&a));
 
     for entry in p_enteries {
-        match entry {
-            DirectoryEntery::Directory {
-                name: n,
-                is_hidden: h,
-            } => {
-                if !h || SHOW_HIDDEN {
-                    print_buf += &format!("\x1b[1;34m{}\x1b[0m  ", n).to_string();
+        if !entry.is_hidden() || SHOW_HIDDEN {
+            match entry.entry_type() {
+                // TODO: Move formatting to function
+                EntryType::Directory => {
+                    if !entry.is_hidden() || SHOW_HIDDEN {
+                        print_buf += &format!("\x1b[1;34m{}\x1b[0m  ", entry.name()).to_string();
+                    }
                 }
-            }
-            DirectoryEntery::File {
-                name: n,
-                is_hidden: h,
-            } => {
-                if !h || SHOW_HIDDEN {
-                    print_buf += &format!("{}  ", n).to_string();
+                _ => {
+                    print_buf += &format!("{}  ", entry.name()).to_string();
                 }
             }
         }
